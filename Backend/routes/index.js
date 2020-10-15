15616faf2a07a3c645a89480138d94223b457c9c
root <root@instance-1.asia-east1-b.c.stubborn-toy.internal>
2020-10-15 08:28:09 +0000

    Finished everything except excel.

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -10,24 +10,32 @@ const sequelize = new Sequelize('AsiaStarUrgent', 'db', '2rjurrru', {
 const db_model = require('./db_model.js');
 const db = db_model(Sequelize, Model, DataTypes, sequelize);
 
-router.post('/submit_problem', function(req, res, next) {
-      
+router.post('/submit_problem', function(req, res) {
+	db.question.create(req.body);
+	res.send("OK");
 });
 
-router.post('/query_cargo_id', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.post('/query_item_id', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/query_car', async function(req, res) {	
+	const ans = await db.car.findAll({ where: req.body })
+	res.send(ans.toJSON());
 });
 
 router.post('/answer_problem', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	db.question.update(
+		  req.body.content,
+		  { where: { id: req.body.id } }
+	)
+	res.send("OK");
 });
 
 router.post('/solve_problem', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var content = req.body.content;
+	if(!content.solve) content.solve_tag = null;
+	db.question.update(
+		content,
+		{ where: { id: req.body.id } }
+	)
+	res.send("OK");
 });
 
 router.post('/submit_car_excel', function(req, res, next) {
