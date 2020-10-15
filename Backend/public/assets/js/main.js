8d8a5819f77dcd70a1bd9f99e54d4e0596293705
root <lawrence910426@gmail.com>
2020-10-15 19:27:10 +0800

    Frontend semi done.

--- Backend/public/assets/js/main.js
+++ Backend/public/assets/js/main.js
@@ -1,5 +1,8 @@
 function gen(row) {
-    return `<div class="row">
+    return `
+<div class="card" style="margin-top:100px">
+    <div class="card-body">
+<div class="row">
     <div class="col">
         <div class="table-responsive">
             <table class="table">
@@ -44,40 +47,28 @@ function gen(row) {
                 </thead>
                 <tbody>
                     <tr>
-                        <td><label></label><textarea id="replyer_name" class="weida_disable" style="height: 30px;width: 100%;" placeholder="[吳邦寧]"></textarea></td>
-                        <td><textarea id="reply" class="weida_disable" style="width: 100%;height: 150px;" placeholder="[Replied]"></textarea>
-                            <div class="d-xl-flex justify-content-xl-end"><button class="btn btn-primary weida_disable" id="submit_answer" type="button">回答</button></div>
+                        <td><label></label><textarea class="weida_disable" style="height: 30px;width: 100%;" placeholder="[吳邦寧]" id="${row.id}_replyer_name">${row.replyer_name == undefined ? "" : row.replyer_name}</textarea></td>
+                        <td><textarea class="weida_disable" style="width: 100%;height: 150px;" placeholder="[Replied]" id="${row.id}_reply">${row.reply == undefined ? "" : row.reply}</textarea>
+                            <div class="d-xl-flex justify-content-xl-end"><button class="btn btn-primary weida_disable submit_answer" self_id="${row.id}" type="button">回答</button></div>
                         </td>
-                        <td><label id="reply_tag">???</label></td>
+                        <td><label id="${row.id}_reply_tag" class="reply_tag">???</label></td>
                         <td>
-                            <div class="form-check"><input type="checkbox" class="form-check-input weida_disable" id="formCheck-1" /><label class="form-check-label" for="formCheck-1">是否解決</label></div>
+                            <div class="form-check"><input id="${row.id}_solve" type="checkbox" class="form-check-input weida_disable solve" self_id="${row.id}" /><label class="form-check-label" for="${row.id}_solve">是否解決</label></div>
                         </td>
-                        <td><label>???</label></td>
+                        <td><label class="solve_tag" id="${row.id}_solve_tag">???</label></td>
                     </tr>
                 </tbody>
             </table>
         </div>
     </div>
 </div>
-<hr />
-`
+</div></div>`
 }
 
 $(document).ready(function() {
-    let githubURL = new URL(window.location.href);
-    let params = githubURL.searchParams;
-    let type = params.get('q'); 
-    if(type == "weida") {
-        $(".asia_star_only").css("display", "none")
-        $(".weida_disable").attr('disabled','disabled');
-    }
-    if(type == "asia") {
-        $(".asia_disable").attr('disabled','disabled');
-    }
-    
     $("input[type=file]").change(function(){
         alert($(this).val());
-    });
+    }); 
     
     $("#submit_question").click(function() {
         var fields = [
@@ -117,32 +108,61 @@ $(document).ready(function() {
         
     })
     
-    $("#submit_answer").click(function() {
-        var fields = [
-            "replyer_name",
-            "reply"
-        ]
-        var submit = {
-            "question_tag": $("#reply_tag").text()
-        }
-        for(var item in fields) {
-            submit[fields[item]] = $(`#${fields[item]}`).val()
-        }
-        $.post( "/answer_problem", submit, function(data) {
-            window.location.reload();
-        });
-    })
-    
-    function update_tag() {
-        $("#question_tag").text(moment().format('YYYY/MM/DD HH:mm:ss'))
-        $("#reply_tag").text(moment().format('YYYY/MM/DD HH:mm:ss'))
-        setTimeout(update_tag, 500)
-    }
-    update_tag();
     
     $.post( "/get_questions", function(data) {
         for(var i in data) {
             $("#history").append(gen(data[i]))
         }
+        hook();
     });
+    
+    function hook() {
+        function update_tag() {
+            $(".question_tag, .reply_tag, .solve_tag, #question_tag ").text(moment().format('YYYY/MM/DD HH:mm:ss'))
+            setTimeout(update_tag, 500)
+        }
+        update_tag();
+
+        let githubURL = new URL(window.location.href);
+        let params = githubURL.searchParams;
+        let type = params.get('q'); 
+        if(type == "weida") {
+            $(".asia_star_only").css("display", "none")
+            $(".weida_disable").attr('disabled','disabled');
+        }
+        if(type == "asia") {
+            $(".weida_only").css("display", "none")
+            $(".asia_disable").attr('disabled','disabled');
+        }
+        
+        $(".submit_answer").off().click(function() {
+            var id = $(this).attr('self_id')
+            var submit = {
+                "id": id,
+                "content": {
+                    "question_tag": $(`#${id}_reply_tag`).text(),
+                    "replyer_name": $(`${id}_replyer_name`).text(),
+                    "reply": $(`${id}_reply`).text()
+                }
+            }
+            $.post("/answer_problem", submit, function(data) {
+                window.location.reload();
+            });
+        })
+        
+        $(".solve").off().click(function() {
+            var id = $(this).attr('self_id')
+            var submit = {
+                "id": id,
+                "content": {
+                    "tag": $(`#${id}_solve_tag`).text(),
+                    "solve": $(`#${id}_solve`).prop("checked") ? 1 : 0 
+                }
+            }
+            $.post("/solve_problem", submit, function(data) {
+                window.location.reload();
+            });
+        })
+    }
+    hook();
 })
